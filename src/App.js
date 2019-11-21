8e0cda18d0710b0910eb996ec8740371e26eeb1c
Román SG <roman.sosa@atos.net>
2019-11-21 15:15:17 +0000

    Improve information shown on violations

--- src/App.js
+++ src/App.js
@@ -8,25 +8,105 @@ const cimi = require('./cimi')
 
 var api = cimi.CimiAPI("/api");
 
+var EMPTY_AGREEMENT = {
+  id: "DEFAULT",
+  details: {
+    name: "",
+    client: {
+      name: "",
+    },
+    provider: {
+      name: "",
+    },
+    guarantees: []
+  }
+}
+var ALL_SERVICEINSTANCE = {}
+
 class ServiceInstancesTable extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { instances: [] }
+    this.state = { 
+      instances: [],
+      agreements: {}
+    }
   }
   render() {
     return(
       <div>
-        <p>Instances</p>
+        <h2>Instances</h2>
         <table>
+          <thead>
+            <tr>
+              <td>Id</td>
+              <td>Client</td>
+              <td>Service Name</td>
+              <td>Type</td>
+              <td>#Violations</td>
+            </tr>
+          </thead>
           <tbody>
-            {this.props.value.map(item => (
+            {this.props.value.instances.map(item => (
               <tr key={item.id}>
                 <td>
-                  <button value={item.id} onClick={ (e) => this.props.onClick(e) }>{ item.id }</button>
+                  <button value={item.id} onClick={ (e) => this.props.onClick(e) }>
+                    { item.id.split('-').pop() }
+                  </button>
                 </td>
+                <td>{this.getAgreement(item.agreement).details.client.name}</td>
+                <td>{this.getAgreement(item.agreement).details.name}</td>
+                <td>{ this.getType(item) }</td>
+                <td>{ this.props.value.nViolations[item.id] }</td>
               </tr>
             ))}
           </tbody>            
+        </table>
+      </div>
+    )
+  }
+  getAgreement(id) {
+    var a = this.props.value.agreementsMap[id];
+    return a === undefined? EMPTY_AGREEMENT : a;
+  }
+
+  getType(si) {
+    return si.service_type;
+  }
+
+}
+
+class AgreementInfo extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = EMPTY_AGREEMENT 
+  }
+
+  render() {
+    let a = this.props.value;
+    let visible = a.id !== EMPTY_AGREEMENT.id;
+    return (
+      <div style={{display: visible? 'block': 'none'}}>
+        <h2>Agreement</h2>
+        <ul>
+          <li><label>Client:</label>{a.details.client.name}</li>
+          <li><label>Provider:</label>{a.details.provider.name}</li>
+        </ul>
+        <h3>Guarantees</h3>
+        <table>
+          <thead>
+            <tr>
+              <td>Name</td>
+              <td>Constraint</td>
+            </tr>
+          </thead>
+          <tbody>
+            {a.details.guarantees.map(item => (
+              <tr key={item.name}>
+                <td>{item.name}</td>
+                <td>{item.constraint}</td>
+              </tr>
+            ))}
+          </tbody>
         </table>
       </div>
     )
@@ -46,13 +126,21 @@ class ViolationsTable extends React.Component {
   render() {
     return (
       <div>
-        <p>Violations</p>
+        <h2>Violations</h2>
         <table>
+          <thead>
+            <tr>
+              <td>Date</td>
+              <td>Guarantee</td>
+              <td>Value</td>
+            </tr>
+          </thead>
           <tbody>
             {this.props.value.map(item => (
               <tr key={item.id}>
-                <td>{item.id}</td>
-                <td>{item.datetime}</td>
+                <td>{this.formatDate(item.datetime)}</td>
+                <td>{item.guarantee}</td>
+                <td>{this.formatValue(item.values)}</td>
               </tr>
             ))}
           </tbody>
@@ -60,22 +148,43 @@ class ViolationsTable extends React.Component {
       </div>
     );
   }
+
+  formatDate(s) {
+    return s;    
+  }
+
+  formatValue(values) {
+    return JSON.stringify(values);
+  }
 }
 
 class App extends React.Component {
   
   constructor(props) {
     super(props);
-    this.state = { instances: [], violations: [], instancesMap: {} }
+    this.state = { 
+      instances: [], 
+      agreements: [],
+      agreement: EMPTY_AGREEMENT,
+      violations: [], 
+      instancesMap: {},
+      agreementsMap: {}
+    }
   }
   async componentDidMount() {
     var r = await getServiceInstances()
+    var a = await getAgreements()
+    var nViolations = await this.buildNViolations(r.serviceInstances)
     console.log(r)
     this.setState( (state, props) => {
       return { 
         instances : r.serviceInstances,
+        agreements : r.agreements,
+        agreement: EMPTY_AGREEMENT,
         violations: [],
-        instancesMap: this.buildInstancesMap(r.serviceInstances)
+        nViolations: nViolations,
+        instancesMap: this.buildInstancesMap(r.serviceInstances),
+        agreementsMap: this.buildAgreementsMap(a.agreements)
       }
     })
   }
@@ -87,11 +196,33 @@ class App extends React.Component {
     }, {});
   }
 
+  buildAgreementsMap(agreements) {
+    var d = {}
+    d[EMPTY_AGREEMENT.id] = EMPTY_AGREEMENT;
+    return agreements.reduce ( (acc, curr) => {
+      acc[curr.id] = curr;
+      return acc;
+    }, d);
+  }
+
+  async buildNViolations(serviceInstances) {
+    var d = {};
+    for (var i = 0; i < serviceInstances.length; i++) {
+      var si = serviceInstances[i];
+      var vs = await getViolations(si);
+      d[si.id] = vs.length;
+    }
+    return d;
+  }
+
   render() {
     return (
       <div className="App">
         <div>
-          <ServiceInstancesTable value={this.state.instances} onClick={(e) => this.handleClick(e)} />
+          <ServiceInstancesTable value={this.state} onClick={(e) => this.handleClick(e)} />
+        </div>
+        <div>
+          <AgreementInfo value={this.state.agreement} />
         </div>
         <div>
           <ViolationsTable value={this.state.violations} />
@@ -109,9 +240,15 @@ class App extends React.Component {
       return {
         instances: state.instances,
         instancesMap: state.instancesMap,
-        violations: vs
+        violations: vs,
+        agreement: this.getAgreement(instance.agreement)
       }
     });
+  }
+
+  getAgreement(id) {
+    let a = this.state.agreementsMap[id];
+    return a === undefined? EMPTY_AGREEMENT : a;
   }
 }
 
@@ -128,7 +265,14 @@ async function getServiceInstances() {
 }
 
 async function getViolations(serviceInstance) {
-  return api.violations(`$filter=(agreement_id/href="${serviceInstance.agreement}")`)
+  var vs = await api.violations(`$filter=(agreement_id/href="${serviceInstance.agreement}")`)    
+  // Removes incorrect violations when using stub CIMI server
+  vs = vs.filter( (item) => { return item.agreement_id.href === serviceInstance.agreement });
+  return vs;
+}
+
+async function getAgreements() {
+  return api.get('agreement')
 }
 
 export default App;
